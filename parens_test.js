eaecc74b1b701ff4a87feccc68e08792e105022d
Jozef <jozefw@sbcglobal.net>
2017-09-30 13:59:27 -0700

    add html tags to append

--- parens_test.js
+++ parens_test.js
@@ -1,24 +1,15 @@
-function balencedParens(string){
-return string.split('').reduce(function(counter,item){
-// increment counter by one if a left facing parens decrement if right facing
-if(item === "("){
-	return counter++;
-
-}
-if (item === ")"){
-	return counter--;
-}
-return counter;
-
-},0);
-
+function balencedParens(string) {
+    return !string.split("").reduce(function(counter, item) {
+        if (item < 0){return counter;}
+        if (item === "(") {return ++counter;}
+        if (item === ")") {return --counter;}
+        return counter;
+    }, 0);
 }
 
-$(document).ready(function(){
-	var text = $("#parens").val();
-	$('button').on("click",function(){
-		balencedParens(text);
-	});
-})
-
-
+    $('button').on("click", function() {
+        var text = $("#parens").val();
+        var result = balencedParens(text);
+        console.log(result);
+        $('.response').append('<h1>' + result +'</h1>');
+    });
