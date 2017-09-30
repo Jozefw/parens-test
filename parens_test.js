0fff88001a14322745ab9fa07c1fcd7915b521d3
Jozef <jozefw@sbcglobal.net>
2017-09-30 12:22:36 -0700

    add logic

--- parens_test.js
+++ parens_test.js
@@ -1,6 +1,14 @@
 function balencedParens(string){
 return string.split('').reduce(function(counter,item){
 // increment counter by one if a left facing parens decrement if right facing
+if(item === "("){
+	return counter++;
+
+}
+else if (item === ")"){
+	return counter--;
+} else {return}
+
 },0);
 
 }
